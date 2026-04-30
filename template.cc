b284e0e3db6d8eccd4040b255c4f01011ec4904e
aaronwangxy <aaronwang.wxy@gmail.com>
2026-04-29 20:12:51 -0400

    update template

--- template.cc
+++ template.cc
@@ -12,9 +12,9 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
-    cin >> tc;
+    // cin >> tc;
     for (int t = 1; t <= tc; t++) {
-        cout << "Case #" << t << ": ";
+        // cout << "Case #" << t << ": ";
         solve();
     }
 }
