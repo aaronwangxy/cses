430e567f1359e9ea2b2ddb79f83c120d8d250f52
aaronwangxy <aaronwang.wxy@gmail.com>
2026-05-02 18:29:41 -0400

    distinct numbers

---
+++ sort_search/distinct_numbers.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vt vector
+
+void solve() {
+    int n;
+    cin >> n;
+    vt<int> arr(n);
+    for (auto &v : arr) cin >> v;
+
+    set<int> s;
+    for (auto v : arr) s.insert(v);
+    cout << s.size();
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
