508843bb4d0cf2bd09ee848e264bfe76acf280af
aaronwangxy <aaronwang.wxy@gmail.com>
2026-05-03 14:54:44 -0400

    coin combinations i

---
+++ dp/coin_combinations_i.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vt vector
+
+const ll MOD = 1e9+7;
+
+void solve() {
+    int n, target;
+    cin >> n >> target;
+    vt<int> coins(n);
+    for (auto &c : coins) cin >> c;
+
+    vt<ll> dp(target+1);
+    dp[0] = 1;
+    for (int i = 1; i <= target; ++i) {
+        for (auto c : coins) {
+            if (i - c >= 0) {
+                dp[i] = (dp[i] + dp[i-c]) % MOD;
+            }
+        }
+    }
+    cout << dp[target];
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
